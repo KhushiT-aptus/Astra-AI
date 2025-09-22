19393e19e4832d9933203f08dcffe004c16961b2
KhushiT-aptus <khushi.thacker@aptusdatalabs.com>
2025-09-22 17:05:57 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [rememberMe, setRememberMe] = useState(false);
+  
 
   const handleLogin = (e) => {
     e.preventDefault();
